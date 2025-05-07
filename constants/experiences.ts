430fa69455c084a70955273670535c3f92d3dbcb
Alex Djonata <31193835+adjonata@users.noreply.github.com>
2025-05-07 11:37:55 -0300

    Update experiences.ts

--- constants/experiences.ts
+++ constants/experiences.ts
@@ -10,7 +10,7 @@ export type Experience = {
 export const experiences: Experience[] = [
   {
     company_name: "Segdev",
-    position: "Desenvolvedor Front-End Pleno",
+    position: "Desenvolvedor Frontend Pleno",
     description:
       "Trabalho desenvolvendo plataformas de seguros para a Telefônica Corretora de Seguros, atuando como voz principal do desenvolvimento Frontend em diversos projetos que abrangem vendas de seguros e emissões de apólices com as seguradoras: Akad, 88i, Zurich e Porto. Possuo também experiência com a corretora Teleport e a plataforma de pagamentos espanhola GEDOC.",
     start_date: "2022-04-01",
@@ -19,7 +19,7 @@ export const experiences: Experience[] = [
   },
   {
     company_name: "DZ Labs",
-    position: "Desenvolvedor Fullstack Pleno",
+    position: "Desenvolvedor Frontend Pleno",
     description:
       "Colaborei com a equipe no desenvolvimento de sistemas de vendas integrados com Shopify e plataformas de pagamento, atuando na integração de APIs e na personalização de funcionalidades conforme as necessidades do cliente.",
     start_date: "2022-01-01",
@@ -28,7 +28,7 @@ export const experiences: Experience[] = [
   },
   {
     company_name: "Codefiction.net",
-    position: "Desenvolvedor Fullstack Pleno",
+    position: "Desenvolvedor Frontend Pleno",
     description:
       "Desenvolvi interfaces para cardápios online (ex. anota.ai) e sistemas de gestão, focando na usabilidade e na experiência do usuário.",
     start_date: "2020-12-01",
@@ -37,7 +37,7 @@ export const experiences: Experience[] = [
   },
   {
     company_name: "Wikidados - Soluções e Desenvolvimentos",
-    position: "Desenvolvedor Front-End",
+    position: "Desenvolvedor Frontend",
     description:
       "Trabalhei no desenvolvimento de um projeto para acompanhar frotas de caminhões e outros veículos, ajudando a monitorar e gerenciar a logística de forma mais prática. Também lidei com a gestão de silos, otimizando o controle dos estoques.",
     start_date: "2020-07-01",
