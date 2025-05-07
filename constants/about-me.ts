401e34d6dad2596c2a528bb1162139fa0cac4b14
Alex Djonata <31193835+adjonata@users.noreply.github.com>
2025-05-07 11:37:11 -0300

    Update about-me.ts

--- constants/about-me.ts
+++ constants/about-me.ts
@@ -3,5 +3,5 @@ export const aboutMe = {
   age: 23,
   position: "Fullstack Developer",
   description:
-    "- Sou um Desenvolvedor Fullstack comprometido em criar aplicações de alta qualidade.",
+    "- Sou um Desenvolvedor Frontend comprometido em criar aplicações de alta qualidade.",
 };
